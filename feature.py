d4b6b2e730afb547215e1daeaac482e965ee9799
Vander Caneppelle <vandercaneppelle@outlook.com>
2023-08-15 09:58:22 -0300

    vander changed

--- feature.py
+++ feature.py
@@ -2,4 +2,7 @@ print("this is a feature")
 
 for i in range(10):
     print(i)
-print ("vander changed")
+print("vander changed")
+print("vander changed")
+print("vander changed")
+print("vander changed")
